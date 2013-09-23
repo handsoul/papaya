329fabe86d1100865aa2dd531cd62c55ad32d982
handsoul <rtertliu@gmail.com>
2013-09-23 23:11:37 +0800

    some changes to make the project compiled under gcc_x86_64

---
+++ include/debug.h
@@ -0,0 +1,13 @@
+#ifndef __debug_h__
+#define __debug_h__
+
+
+#include <valType.h>
+#include <elf.h>
+void init_debug(void);
+void debug_watch(u32 ,int);
+
+void kernelsection(void);
+
+
+#endif //__debug_h__
